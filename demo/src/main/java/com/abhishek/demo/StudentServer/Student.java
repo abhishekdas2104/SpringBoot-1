cd6bb1249e4cacebf070eddffaadbd147b145217
abhishekdas2104 <realservicek23@gmail.com>
2026-07-14 14:32:31 +0530

    Student Server

---
+++ demo/src/main/java/com/abhishek/demo/StudentServer/Student.java
@@ -0,0 +1,41 @@
+package com.abhishek.demo.StudentServer;
+
+public class Student {
+
+    int Id;
+    String name;
+    int age;
+    String department;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
---
+++ demo/src/main/java/com/abhishek/demo/StudentServer/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.abhishek.demo.StudentServer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
