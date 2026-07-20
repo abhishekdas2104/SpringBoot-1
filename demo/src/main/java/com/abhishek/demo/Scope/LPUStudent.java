7a8bbeaf1f6d1fa72d9324da9e984b9b600a72d6
abhishekdas2104 <realservicek23@gmail.com>
2026-07-20 14:52:26 +0530

    Scope

---
+++ demo/src/main/java/com/abhishek/demo/Scope/LPUStudent.java
@@ -0,0 +1,12 @@
+package com.abhishek.demo.Scope;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LPUStudent {
+
+    @Autowired
+    LPU lpu;
+
+}
